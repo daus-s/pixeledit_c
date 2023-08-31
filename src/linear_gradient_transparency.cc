40de618f73d99002f6c358e1820ba959f7976f3b
daus-s <dauscarmichael@gmail.com>
2023-08-31 01:58:44 -0700

    new files

---
+++ src/linear_gradient_transparency.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <png.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv) 
+{
+    // Your code goes here
+    
+    printf("opening %s...\n", argv[1]);
+
+
+
+    return 0;
+}
